d78effb600e33b6fa1de04a0a8307b8f57e91167
shahiadarsh <adarsh.shahi@completrix.com>
2025-10-29 13:05:57 +0530

    Updating cors

--- server.js
+++ server.js
@@ -22,10 +22,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "https://auto-parts-frontend-sand.vercel.app",
+      "http://localhost:5173", // React dev server
+      "http://localhost:3000"  // optional: Next.js or other local setup
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
